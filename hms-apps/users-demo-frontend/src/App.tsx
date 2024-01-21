3c7282e71acac3fd7f85390fceacea3493169367
nwannelol <nwanneegbuna3@gmail.com>
2024-01-22 00:01:07 +0100

    nwannes commit

--- hms-apps/users-demo-frontend/src/App.tsx
+++ hms-apps/users-demo-frontend/src/App.tsx
@@ -33,103 +33,128 @@ type Props = {
   };
 }
 
-export const AppContext = createContext<AppContextType | null>(null);
 
-const App: React.FC<Props> = ({ assetMap }) => {
-  const queryClient = new QueryClient();
-  const { i18n } = useTranslation();
 
-  const changeI18nLanguageToClientPreferred = async () => {
-    if (i18n.language !== assetMap?.clientFirstAcceptLanguage) {
-      await i18n.changeLanguage(assetMap?.clientFirstAcceptLanguage);
-    }
-  };
+function App() {
+  const baseUrl = "/v1";
+  //create a react query client at the top
+  // Create a client
+  const queryClient = new QueryClient()
 
-  useEffect(() => {
-    // Check if assetMap sent in production mode; if not, redirect to a proper ssr endpoint.
-    if (!DEV_MODE) {
-      // Attempt to change language here to locale
-      changeI18nLanguageToClientPreferred();
-      if (!assetMap) {
-        window.location.href = '/web'; // Simulate a mouse click
-      }
-    }
-  }, [i18n.language, assetMap]);
-
-  const appBody = () => {
-    // Can be used at DEV time and PROD time
-
-    // Default settings on dev mode
-    let baseUrl = '/v1';
-    let title = 'Hello World';
-
-    if (assetMap) {
-      // Prod mode. Sent by ssr endpoint.
-      baseUrl = assetMap.baseUrl;
-      title = assetMap.initialContentMap.title!;
-    }
-
-    return (
-      // Provide the client to your App
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <Routes>
-            <Route path={baseUrl} element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="view-users" element={<Users />} />
-            </Route>
-          </Routes>
-        </ThemeProvider>
-      </QueryClientProvider>
-    )
-  };
-
-  const additionStyles = () => {
-    if (assetMap) {
-      return assetMap['additional-styles'].map((additionalStyle) => (
-        <link rel="stylesheet" href={additionalStyle} key={additionalStyle} />
-      ));
-    }
-    return null;
-  };
-
-  const additionalJss = () => {
-    if (assetMap) {
-      return assetMap['additional-jss'].map((additionalJs) => (
-        <script src={additionalJs} key={additionalJs} />
-      ));
-    }
-    return null;
-  };
-
-  const output = () => {
-    if (assetMap) {
-      return (
-        <html>
-          <head>
-            <link rel="stylesheet" href={assetMap['styles.css']} />
-            {additionStyles()}
-            {assetMap['manifest'] && <link rel="manifest" href={assetMap['manifest']}></link>}
-            {assetMap['vite-plugin-pwa:register-sw'] && (
-              <script id="vite-plugin-pwa:register-sw" src="/registerSW.js"></script>
-            )}
-            {assetMap.initialContentMap && <title>{assetMap.initialContentMap['title']}</title>}
-          </head>
-          {appBody()}
-          {additionalJss()}
-        </html>
-      );
-    } else {
-      return <>{appBody()}</>;
-    }
-  };
-
-  return <>{output()}</>;
-};
-
-const UseParams: React.FC = () => {
-  let { name } = useParams();
-  return <h3>Hello {name}</h3>;
-};
+  return (
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Routes>
+          <Route path={`${baseUrl}`} element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="view-users" element={<Users />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
 
 export default App
+
+// export const AppContext = createContext<AppContextType | null>(null);
+
+// const App: React.FC<Props> = ({ assetMap }) => {
+//   const queryClient = new QueryClient();
+//   const { i18n } = useTranslation();
+
+//   const changeI18nLanguageToClientPreferred = async () => {
+//     if (i18n.language !== assetMap?.clientFirstAcceptLanguage) {
+//       await i18n.changeLanguage(assetMap?.clientFirstAcceptLanguage);
+//     }
+//   };
+
+//   useEffect(() => {
+//     // Check if assetMap sent in production mode; if not, redirect to a proper ssr endpoint.
+//     if (!DEV_MODE) {
+//       // Attempt to change language here to locale
+//       changeI18nLanguageToClientPreferred();
+//       if (!assetMap) {
+//         window.location.href = '/web'; // Simulate a mouse click
+//       }
+//     }
+//   }, [i18n.language, assetMap]);
+
+//   const appBody = () => {
+//     // Can be used at DEV time and PROD time
+
+//     // Default settings on dev mode
+//     let baseUrl = '/v1';
+//     let title = 'Hello World';
+
+//     if (assetMap) {
+//       // Prod mode. Sent by ssr endpoint.
+//       baseUrl = assetMap.baseUrl;
+//       title = assetMap.initialContentMap.title!;
+//     }
+
+//     return (
+//       // Provide the client to your App
+//       <QueryClientProvider client={queryClient}>
+//         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+//           <Routes>
+//             <Route path={baseUrl} element={<Layout />}>
+//               <Route index element={<Home />} />
+//               <Route path="view-users" element={<Users />} />
+//             </Route>
+//           </Routes>
+//         </ThemeProvider>
+//       </QueryClientProvider>
+//     )
+//   };
+
+//   const additionStyles = () => {
+//     if (assetMap) {
+//       return assetMap['additional-styles'].map((additionalStyle) => (
+//         <link rel="stylesheet" href={additionalStyle} key={additionalStyle} />
+//       ));
+//     }
+//     return null;
+//   };
+
+//   const additionalJss = () => {
+//     if (assetMap) {
+//       return assetMap['additional-jss'].map((additionalJs) => (
+//         <script src={additionalJs} key={additionalJs} />
+//       ));
+//     }
+//     return null;
+//   };
+
+//   const output = () => {
+//     if (assetMap) {
+//       return (
+//         <html>
+//           <head>
+//             <link rel="stylesheet" href={assetMap['styles.css']} />
+//             {additionStyles()}
+//             {assetMap['manifest'] && <link rel="manifest" href={assetMap['manifest']}></link>}
+//             {assetMap['vite-plugin-pwa:register-sw'] && (
+//               <script id="vite-plugin-pwa:register-sw" src="/registerSW.js"></script>
+//             )}
+//             {assetMap.initialContentMap && <title>{assetMap.initialContentMap['title']}</title>}
+//           </head>
+//           {appBody()}
+//           {additionalJss()}
+//         </html>
+//       );
+//     } else {
+//       return <>{appBody()}</>;
+//     }
+//   };
+
+//   return <>{output()}</>;
+// };
+
+// const UseParams: React.FC = () => {
+//   let { name } = useParams();
+//   return <h3>Hello {name}</h3>;
+// };
+
+//xport default App
